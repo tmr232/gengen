e1091431e2788686f70922eb8b8cf1028457ebeb
Tamir Bahar <tbahar@vdoo.com>
2022-05-20 13:59:35 +0300

    Define the generator function type, so that we can define generators!

--- gengen/real_syntax.go
+++ gengen/real_syntax.go
@@ -9,3 +9,24 @@ type Generator[T any] interface {
 	Value() T
 	Error() error
 }
+
+type GeneratorFunction[T any] struct {
+	Advance func() (hasValue bool, value T, err error)
+	value   T
+	err     error
+}
+
+func (g *GeneratorFunction[T]) Next() bool {
+	hasValue, value, err := g.Advance()
+	g.value = value
+	g.err = err
+	return hasValue
+}
+
+func (g *GeneratorFunction[T]) Value() T {
+	return g.value
+}
+
+func (g *GeneratorFunction[T]) Error() error {
+	return g.err
+}
