03b0e23e1d66850e43fc6333af86b940717e2d59
Tamir Bahar <tamir.bahar@gmail.com>
2022-05-24 17:42:50 +0300

    Allow generators to use generators.

--- gengen/fake_synax.go
+++ gengen/fake_synax.go
@@ -5,3 +5,7 @@ package gengen
 func Yield(value any) {}
 
 type Generator[T any] error
+
+func (g Generator[T]) Next() bool   { return true }
+func (g Generator[T]) Value() T     { return *new(T) }
+func (g Generator[T]) Error() error { return nil }
