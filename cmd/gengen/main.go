0fc95fe4beebb33ec9f131679cf6ac2de4b7f6a0
Tamir Bahar <tamir.bahar@gmail.com>
2022-06-26 19:18:03 +0300

    Removed unused code.

--- cmd/gengen/main.go
+++ cmd/gengen/main.go
@@ -11,7 +11,6 @@ import (
 	"golang.org/x/tools/go/packages"
 	"io/ioutil"
 	"log"
-	"path"
 	"reflect"
 	"strings"
 )
@@ -92,65 +91,6 @@ func IsGenerator(pkg *packages.Package, fdecl *ast.FuncDecl) (result bool) {
 	ast.Walk(visitor, fdecl)
 	return result
 }
-func getGeneratorDefinitions(dir string, tags []string) []generatorDecls {
-	cfg := &packages.Config{
-		Mode:       packages.NeedTypes | packages.NeedTypesInfo | packages.NeedFiles | packages.NeedSyntax | packages.NeedName | packages.NeedImports,
-		Context:    nil,
-		Logf:       nil,
-		Dir:        dir,
-		Env:        nil,
-		BuildFlags: []string{fmt.Sprintf("-tags=%s", strings.Join(tags, " "))},
-		Fset:       nil,
-		ParseFile:  nil,
-		Tests:      true,
-		Overlay:    nil,
-	}
-
-	pkgs, err := packages.Load(cfg)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var generatorFunctionDefs []generatorDecls
-	for _, pkg := range pkgs {
-		var decls []*ast.FuncDecl
-		for _, f := range pkg.Syntax {
-			for _, comment := range f.Comments {
-				ast.Print(pkg.Fset, comment)
-			}
-			//ast.Print(pkg.Fset, f)
-			/*
-				Since this works - the next thing to do is replace the current generation mechanism.
-				We need to visit the entire File AST.
-				If the file does generator-generation (probably best to check the build tag?)
-				we need to copy it over, and generate for it.
-				As fot the generation - we just visit the AST and print it.
-				If we encounter a function definition, we send it to our generator printer.
-				Otherwise - we format it as regular code.
-			*/
-			for _, decl := range f.Decls {
-				switch decl := decl.(type) {
-				case *ast.FuncDecl:
-					if !IsGenerator(pkg, decl) {
-						continue
-					}
-
-					fmt.Println(pkg.Fset.Position(decl.Pos()).Filename)
-
-					decls = append(decls, decl)
-				}
-			}
-		}
-		if len(decls) > 0 {
-			generatorFunctionDefs = append(generatorFunctionDefs, generatorDecls{
-				pkg:   pkg,
-				decls: decls,
-			})
-		}
-	}
-
-	return generatorFunctionDefs
-}
 
 func formatSource(src []byte) []byte {
 	formattedSrc, err := format.Source(src)
@@ -189,51 +129,6 @@ func (imports Imports) String() string {
 	return out.String()
 }
 
-func collectImports(pkg *packages.Package) Imports {
-	var imports Imports
-	for _, f := range pkg.Syntax {
-		for _, imp := range f.Imports {
-			if imp.Name != nil {
-				imports = append(imports, ImportLine{Name: &imp.Name.Name, Path: imp.Path.Value})
-			} else {
-				imports = append(imports, ImportLine{Path: imp.Path.Value})
-			}
-		}
-	}
-	return imports
-}
-
-type ImportPathsVisitor struct {
-	paths map[string]bool
-	pkg   *packages.Package
-}
-
-func (v ImportPathsVisitor) Visit(n ast.Node) ast.Visitor {
-	ident, isIdent := n.(*ast.Ident)
-	if isIdent {
-		object, exists := v.pkg.TypesInfo.Uses[ident]
-		if exists && object.Pkg() != nil {
-			v.paths[object.Pkg().Path()] = true
-		}
-		return nil
-	}
-	return v
-}
-
-func FindUsedImports(genDef generatorDecls) map[string]bool {
-	paths := make(map[string]bool)
-	visitor := ImportPathsVisitor{
-		paths: paths,
-		pkg:   genDef.pkg,
-	}
-
-	for _, node := range genDef.decls {
-		ast.Walk(visitor, node)
-	}
-
-	return paths
-}
-
 func isGeneratorSourceFile(file *ast.File) bool {
 	if len(file.Comments) == 0 || len(file.Comments[0].List) == 0 {
 		return false
@@ -254,7 +149,7 @@ func getFileImports(file *ast.File) Imports {
 	return imports
 }
 
-func fullFileGenerator() {
+func main() {
 	dir := "."
 	tags := []string{"gengen"}
 
@@ -313,7 +208,6 @@ func fullFileGenerator() {
 					}
 					return false
 				default:
-					fmt.Println(reflect.TypeOf(node))
 					format.Node(&out, pkg.Fset, node)
 					out.WriteString("\n")
 					return false
@@ -323,94 +217,10 @@ func fullFileGenerator() {
 			src := formatSource(out.Bytes())
 
 			filepath := pkg.Fset.Position(file.Pos()).Filename + "_gengen.go"
-			//fmt.Println(string(src))
-			//fmt.Println(filepath)
 			err = ioutil.WriteFile(filepath, src, 0644)
 			if err != nil {
 				log.Fatalf("writing output: %s", err)
 			}
 		}
 	}
-}
-
-func main() {
-	dir := "."
-	buildTag := "gengen"
-
-	generatorDefs := getGeneratorDefinitions(dir, []string{buildTag})
-	for _, genDecls := range generatorDefs {
-		fmt.Println(genDecls.pkg.ID, genDecls.pkg.Name)
-		for _, fdef := range genDecls.decls {
-			fmt.Println("    ", fdef.Name.Name)
-		}
-	}
-
-	wiz := NewWizard()
-	if wiz == nil {
-		log.Fatal("Failed to initialize wizard.")
-	}
-
-	for _, genDef := range generatorDefs {
-		finalImports := CollectUsedImports(genDef)
-		functions := wiz.WithPackage(genDef.pkg, finalImports).convertFunctions(genDef)
-		src, err := wiz.Render("package",
-			struct {
-				PackageName string
-				Imports     Imports
-				Functions   []string
-			}{
-				PackageName: genDef.pkg.Name,
-				Imports:     finalImports,
-				Functions:   functions,
-			})
-		src = formatSource(src)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		filepath := path.Join(dir, genDef.pkg.Name+"_gengen.go")
-		err = ioutil.WriteFile(filepath, src, 0644)
-		if err != nil {
-			log.Fatalf("writing output: %s", err)
-		}
-		//fmt.Println(string(src))
-	}
-
-	sample := sampleGenerator()
-	for sample.Next() {
-		fmt.Println(sample.Value())
-	}
-	if sample.Error() != nil {
-		fmt.Println("Oh no! Error!")
-	}
-
-	fullFileGenerator()
-}
-
-func CollectUsedImports(genDef generatorDecls) Imports {
-	// First, find all the imports
-	imports := collectImports(genDef.pkg)
-	// Then, all the imports used by the generators
-	usedImports := FindUsedImports(genDef)
-	// And only use the intersection!
-	var finalImports Imports
-	for _, importLine := range imports {
-		if usedImports[strings.Trim(importLine.Path, "\"")] {
-			finalImports = append(finalImports, importLine)
-		}
-	}
-
-	fmt.Println("Used Imports ", finalImports)
-	return finalImports
-}
-
-func sampleGenerator() gengen.Generator[int] {
-	done := false
-	return &gengen.GeneratorFunction[int]{Advance: func() (hasValue bool, value int, err error) {
-		if done {
-			return
-		}
-		done = true
-		return true, 42, nil
-	}}
 }
